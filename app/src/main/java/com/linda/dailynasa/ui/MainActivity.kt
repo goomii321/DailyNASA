16068371381c4e6e919c3650f853f24d1e63a0e3
CHUN YU LAI <49115858+goomii321@users.noreply.github.com>
2022-06-09 16:45:06 +0800

    優化drawer

--- app/src/main/java/com/linda/dailynasa/ui/MainActivity.kt
+++ app/src/main/java/com/linda/dailynasa/ui/MainActivity.kt
@@ -2,18 +2,25 @@ package com.linda.dailynasa.ui
 
 import android.os.Bundle
 import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.linda.dailynasa.R
 import com.linda.dailynasa.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var mDrawerToggle:ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,21 +29,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
+        setDrawer()
+        setupNavController()
+    }
 
-        val drawerLayout = binding.drawerLayout
+    private fun setDrawer() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
         appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
+            navController.graph, binding.drawerLayout
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.drawerLayoutMenu.navView,navController)
+
+        mDrawerToggle =
+            ActionBarDrawerToggle(this, binding.drawerLayout
+                , binding.appBarMain.toolbar, R.string.open, R.string.close)
+
+        binding.drawerLayout.addDrawerListener(mDrawerToggle)
+        mDrawerToggle.syncState()
+    }
+
+    private fun setupNavController() {
+        findNavController(R.id.nav_host_fragment_content_main).addOnDestinationChangedListener {
+                navController: NavController, _: NavDestination, _: Bundle? ->
+            when (navController.currentDestination?.id) {
+                R.id.nav_home -> {}
+                R.id.nav_gallery -> {}
+                R.id.nav_slideshow -> {}
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+
+//            viewModel.currentFragmentType.value = when (navController.currentDestination?.id) {
+//                R.id.homeFragment -> CurrentFragmentType.HOME
+//                R.id.diariesFragment -> CurrentFragmentType.DIARY
+//                R.id.storesFragment -> CurrentFragmentType.STORES
+//                R.id.profileFragment -> CurrentFragmentType.PROFILE
+//                R.id.addDiaryFragment -> CurrentFragmentType.ADD
+//                R.id.templateFragment -> CurrentFragmentType.TEMPLATE
+//                else -> viewModel.currentFragmentType.value
+//            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
+//        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
