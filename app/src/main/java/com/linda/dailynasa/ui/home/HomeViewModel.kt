b30f2de28232575013a8ae06a25d28a87bfbd521
CHUN YU LAI <49115858+goomii321@users.noreply.github.com>
2022-06-10 16:00:31 +0800

    get data from api

--- app/src/main/java/com/linda/dailynasa/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/linda/dailynasa/ui/home/HomeViewModel.kt
@@ -1,10 +1,45 @@
 package com.linda.dailynasa.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.linda.dailynasa.common.Logger
+import com.linda.dailynasa.common.Resource
+import com.linda.dailynasa.domain.DailyNasaRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    val repository: DailyNasaRepository
+) : ViewModel() {
 
+    private val viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.IO)
+
+    private val _errorMsg = MutableLiveData<String>()
+    val errorMsg:LiveData<String> = _errorMsg
+
+    fun getApod(input:String?) {
+        val date = input ?: ""
+        coroutineScope.launch {
+            repository.getApodByDate(date)
+                .collect{ result ->
+                    when(result) {
+                        is Resource.Success -> {
+                            Logger.v("result = ${result.data}")
+                        }
+                        is Resource.Error -> {
+                            Logger.e("error = ${result.message}")
+                            _errorMsg.value = result.message ?: "unknown error"
+                        }
+                        is Resource.Loading -> {}
+                    }
+                }
+        }
+    }
 }
--- app/src/main/java/com/linda/dailynasa/ui/home/child/ApodFragment.kt
+++ app/src/main/java/com/linda/dailynasa/ui/home/child/ApodFragment.kt
@@ -5,17 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.linda.dailynasa.databinding.FragmentApodBinding
+import com.linda.dailynasa.ui.home.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ApodFragment : Fragment() {
 
     private lateinit var binding:FragmentApodBinding
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentApodBinding.inflate(inflater,container,false)
+        viewModel.getApod("")
         return binding.root
     }
 
