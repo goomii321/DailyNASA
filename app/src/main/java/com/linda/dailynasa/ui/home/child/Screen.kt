35aff436a9c8ca177e11b2ff5f1f09a3cd0c747c
CHUN YU LAI <49115858+goomii321@users.noreply.github.com>
2022-06-14 16:35:18 +0800

    update gradle
    added compose

---
+++ app/src/main/java/com/linda/dailynasa/ui/home/child/Screen.kt
@@ -0,0 +1,12 @@
+package com.linda.dailynasa.ui.home.child
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview
+@Composable
+fun Test() {
+    Text(text = "Hello 123")
+}
