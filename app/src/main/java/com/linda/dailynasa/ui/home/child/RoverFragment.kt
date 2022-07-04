41e2e47ca200b2ea641b5fc088a1e747e96af781
CHUN YU LAI <49115858+goomii321@users.noreply.github.com>
2022-07-04 09:01:10 +0800

    Update RoverFragment.kt

--- app/src/main/java/com/linda/dailynasa/ui/home/child/RoverFragment.kt
+++ app/src/main/java/com/linda/dailynasa/ui/home/child/RoverFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
